e42bb1b971a4b8425b31f54770c1b2e03b3c8e1d
kevin Leo <kevmanyonge@gmail.com>
2024-07-25 18:43:23 +0300

    footer update

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="copyright-wrapper">
     <div class="copyright-area">
-        <p>{{ __("Developed by") }} <a href="https://appdevs.net">AppDevs</a></p>
-        <p><span>v{{ $basic_settings->web_version ?? "1.0.0" }}</span></p>
+        <p>{{ __("Developed by") }} <a href="https://appdevs.net">Swift Apps Africa</a></p>
+        <p><span>v{{ $basic_settings->web_version ?? "10.0.1" }}</span></p>
     </div>
 </div>
