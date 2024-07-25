314bef4e30f9a0abbb7e948e60c8f305506d4219
kevin Leo <kevmanyonge@gmail.com>
2024-07-25 10:51:06 +0300

    Ndio kudonjo

---
+++ app/Http/Controllers/GlobalController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Helpers\Response;
+use Illuminate\Support\Carbon;
+use Jenssegers\Agent\Facades\Agent;
+use Illuminate\Support\Facades\Validator;
+
+class GlobalController extends Controller
+{
+
+    /**
+     * Funtion for get state under a country
+     * @param country_id
+     * @return json $state list
+     */
+    public function getStates(Request $request) {
+        $request->validate([
+            'country_id' => 'required|integer',
+        ]);
+        $country_id = $request->country_id;
+        // Get All States From Country
+        $country_states = get_country_states($country_id);
+        return response()->json($country_states,200);
+    }
+
+
+    public function getCities(Request $request) {
+        $request->validate([
+            'state_id' => 'required|integer',
+        ]);
+
+        $state_id = $request->state_id;
+        $state_cities = get_state_cities($state_id);
+
+        return response()->json($state_cities,200);
+        // return $state_id;
+    }
+
+
+    public function getCountries(Request $request) {
+        $countries = get_all_countries();
+
+        return response()->json($countries,200);
+    }
+
+
+    public function getTimezones(Request $request) {
+        $timeZones = get_all_timezones();
+
+        return response()->json($timeZones,200);
+    }
+
+    public function userInfo(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'text'      => "required|string",
+        ]);
+
+        if($validator->fails()) {
+            return Response::error($validator->errors(),null,400);
+        }
+
+        $validated = $validator->validate();
+
+        $field_name = "username";
+        if(check_email($validated['text'])) {
+            $field_name = "email";
+        }
+
+        try{
+            $user = User::notAuth()->where($field_name,$validated['text'])->first();
+        }catch(Exception $e) {
+            $error = ['error' => ['Something went wrong!. Please try again.']];
+            return Response::error($error,null,500);
+        }
+
+        $success = ['success' => ['User available']];
+        return Response::success($success,$user,200);
+    }
+
+    /**
+     * Frontend cookie
+     */
+    public function setCookie(Request $request){
+        $userAgent = $request->header('User-Agent');
+        $cookie_status = $request->type;
+        if($cookie_status == 'allow'){
+            $response_message = __("Cookie Allowed Success");
+            $expirationTime = 2147483647; //Maximum Unix timestamp.
+        }else{
+            $response_message = __("Cookie Declined");
+            $expirationTime = Carbon::now()->addHours(24)->timestamp;// Set the expiration time to 24 hours from now.
+        }
+        $browser = Agent::browser();
+        $platform = Agent::platform();
+        $ipAddress = $request->ip();
+        return response($response_message)->cookie('approval_status', $cookie_status,$expirationTime)
+                                            ->cookie('user_agent', $userAgent,$expirationTime)
+                                            ->cookie('ip_address', $ipAddress,$expirationTime)
+                                            ->cookie('browser', $browser,$expirationTime)
+                                            ->cookie('platform', $platform,$expirationTime);
+    }
+}
---
+++ resources/views/partials/footer-asset.blade.php
@@ -0,0 +1,59 @@
+
+<!-- jquery -->
+<script src="{{ asset('public/frontend/js/jquery-3.6.0.min.js') }}"></script>
+<!-- bootstrap js -->
+<script src="{{ asset('public/frontend/js/bootstrap.bundle.min.js') }}"></script>
+<!-- swipper js -->
+<script src="{{ asset('public/frontend/js/swiper.min.js') }}"></script>
+<!-- lightcase js-->
+<script src="{{ asset('public/frontend/js/lightcase.js') }}"></script>
+<!-- smooth scroll js -->
+<script src="{{ asset('public/frontend/js/smoothscroll.min.js') }}"></script>
+<script src="{{ asset('public/frontend/js/jquery.nice-select.js') }}"></script>
+
+<script src="{{ asset('public/frontend/js/apexcharts.min.js') }}"></script>
+
+<!-- Select 2 JS -->
+<script src="{{ asset('public/backend/js/select2.min.js') }}"></script>
+<script src="{{ asset('public/backend/library/popup/jquery.magnific-popup.js') }}"></script>
+
+@include('admin.partials.notify')
+
+<!-- main -->
+<script src="{{ asset('public/frontend/js/main.js') }}"></script>
+
+{{-- Cookie Script START --}}
+<script>
+    (function ($) {
+        "use strict";
+
+        //Allow
+        $('.cookie-btn').on('click', function() {
+            var postData = {
+                type: "allow",
+                _token: laravelCsrf(),
+            };
+            $.post('{{ route('global.set.cookie') }}', postData, function(response) {
+                throwMessage('success', [response]);
+                setTimeout(function() {
+                    location.reload();
+                }, 1000);
+            });
+        });
+
+        //Decline
+        $('.cookie-btn-cross').on('click', function() {
+            var postData = {
+                type: "decline",
+                _token: laravelCsrf(),
+            };
+            $.post('{{ route('global.set.cookie') }}', postData, function(response) {
+                throwMessage('error',[response]);
+                setTimeout(function(){
+                    location.reload();
+                },1000);
+            });
+        });
+    })(jQuery)
+</script>
+{{-- Cookie Script END --}}
